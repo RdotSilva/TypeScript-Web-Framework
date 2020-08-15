0367a3554ad9f15d3eb2e407e4c3d9ffc8a4cce9
RdotSilva <RyandotSilva@gmail.com>
2020-08-15 19:30:55 -0400

    Add test case for updating user data individually

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,10 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Ryan", age: 34 });
 
+// Here we can update each piece of data individually
+// This is allowed because we added a ? to the UserProps interface to make them optional.
+user.set({ name: "NewRyan" });
+user.set({ age: 999 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
