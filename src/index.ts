cdbd0a2c50f8a92b9a966447ec407d69f07ebda6
RdotSilva <RyandotSilva@gmail.com>
2020-08-15 19:23:15 -0400

    Add test to get user data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello Web Framework");
+import { User } from "./models/User";
+
+const user = new User({ name: "Ryan", age: 34 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
