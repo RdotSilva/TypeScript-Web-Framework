a1b48be6ec9dc4d0eafdb79577b5920f3bf70bb5
RdotSilva <RyandotSilva@gmail.com>
2020-08-15 19:34:04 -0400

    Add JSDoc comments

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,35 @@
+/**
+ * Interface listing the properties that a user should have
+ * @interface UserProps
+ */
 interface UserProps {
   name?: string;
   age?: number;
 }
 
+/**
+ * Class that represents a User
+ * @export
+ * @class User
+ */
 export class User {
   constructor(private data: UserProps) {}
 
+  /**
+   * Get data related to User
+   * @param {string} propName - The property name you want to retrieve
+   * @returns {(number | string)}
+   * @memberof User
+   */
   get(propName: string): number | string {
     return this.data[propName];
   }
 
+  /**
+   * Update User data
+   * @param {UserProps} update - The update you want to make
+   * @memberof User
+   */
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
