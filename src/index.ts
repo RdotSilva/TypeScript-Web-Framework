3bea6ed26a58564bcec3c596f3aab5de1288a205
RdotSilva <RyandotSilva@gmail.com>
2020-08-16 15:20:07 -0400

    Add on method test case

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Ryan", age: 34 });
 
+// Testing the on method to add event callbacks
+user.on("change", () => {});
+user.on("change", () => {});
+user.on("click", () => {});
+
 // Here we can update each piece of data individually
 // This is allowed because we added a ? to the UserProps interface to make them optional.
 user.set({ name: "NewRyan" });
@@ -9,3 +14,5 @@ user.set({ age: 999 });
 
 console.log(user.get("name"));
 console.log(user.get("age"));
+
+console.log(user);
