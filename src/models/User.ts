1ca9c83577a35da97091a636e81a2687ff45f51b
RdotSilva <RyandotSilva@gmail.com>
2020-08-16 15:22:35 -0400

    Add trigger method

--- src/models/User.ts
+++ src/models/User.ts
@@ -49,4 +49,22 @@ export class User {
     handlers.push(callback);
     this.events[eventName] = handlers;
   }
+
+  /**
+   * Trigger all callbacks for an event
+   * @param {string} eventName - Event to trigger callbacks
+   * @returns {void}
+   * @memberof User
+   */
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
 }
