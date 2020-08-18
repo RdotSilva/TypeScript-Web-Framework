510286510cf904e457283ad11b0863bc2b8526c3
RdotSilva <RyandotSilva@gmail.com>
2020-08-17 20:05:12 -0400

    Add test case for event triggers

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import { User } from "./models/User";
 const user = new User({ name: "Ryan", age: 34 });
 
 // Testing the on method to add event callbacks
-user.on("change", () => {});
-user.on("change", () => {});
-user.on("click", () => {});
-
-// TODO: Add test case to test event triggering method
+user.on("change", () => {
+  console.log("Change #1");
+});
+user.on("change", () => {
+  console.log("Change #2");
+});
+user.on("click", () => {
+  console.log("Save was triggered");
+});
 
 // Here we can update each piece of data individually
 // This is allowed because we added a ? to the UserProps interface to make them optional.
@@ -17,4 +21,7 @@ user.set({ age: 999 });
 console.log(user.get("name"));
 console.log(user.get("age"));
 
-console.log(user);
+// Tests event triggers
+user.trigger("change");
+user.trigger("save");
+user.trigger("");
