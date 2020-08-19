6c46d24f276a467585fa9b921a3e2bcb0bb0577f
RdotSilva <RyandotSilva@gmail.com>
2020-08-18 20:59:30 -0400

    Add JSDoc comments

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -3,12 +3,24 @@ type Callback = () => void;
 export class Eventing {
   events: { [key: string]: Callback[] } = {};
 
+  /**
+   * Add a callback function to an event
+   * @param {string} eventName - Event name to update
+   * @param {Callback} callback - Callback function to add to event
+   * @memberof Eventing
+   */
   on(eventName: string, callback: Callback): void {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
     this.events[eventName] = handlers;
   }
 
+  /**
+   * Trigger all callback functions for an event
+   * @param {string} eventName - Event to trigger callbacks
+   * @returns {void}
+   * @memberof Eventing
+   */
   trigger(eventName: string): void {
     const handlers = this.events[eventName];
 
