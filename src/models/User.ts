7a8066a1b137a665d44acb0a149418469ede92c6
RdotSilva <RyandotSilva@gmail.com>
2020-08-18 20:58:22 -0400

    Remove Eventing code

--- src/models/User.ts
+++ src/models/User.ts
@@ -10,16 +10,12 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 /**
  * Class that represents a User
  * @export
  * @class User
  */
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   /**
@@ -39,36 +35,6 @@ export class User {
    */
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  /**
-   * Add a Callback function to an event
-   * @param {string} eventName - Event name to update
-   * @param {Callback} callback - Callback function to add to event
-   * @memberof User
-   */
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  /**
-   * Trigger all callbacks for an event
-   * @param {string} eventName - Event to trigger callbacks
-   * @returns {void}
-   * @memberof User
-   */
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
   }
 
   fetch(): void {
