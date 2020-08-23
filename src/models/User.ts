2972039ec34bdfbb8b9eca993579ece505f3330c
RdotSilva <RyandotSilva@gmail.com>
2020-08-22 20:29:14 -0400

    Change on method to getter

--- src/models/User.ts
+++ src/models/User.ts
@@ -28,7 +28,7 @@ export class User {
     this.attributes = new Attributes<UserProps>(attrs);
   }
 
-  on(eventName: string, callback: Callback): void {
-    this.events.on(eventName, callback);
+  get on() {
+    return this.events.on;
   }
 }
