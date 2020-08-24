7cb3343a08cb2db1e1628e4b22a2bef25ea730c3
RdotSilva <RyandotSilva@gmail.com>
2020-08-24 18:01:25 -0400

    Add ModelAttributes interface

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,3 +1,9 @@
+interface ModelAttributes<T> {
+  set(update: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
+
 interface Events {
   on(eventName: string, callback: () => void): void;
   trigger(eventName: string): void;
