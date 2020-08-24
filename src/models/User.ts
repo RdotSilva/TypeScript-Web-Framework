cc7f8d05db45d1507f73188e39f4dea012207d57
RdotSilva <RyandotSilva@gmail.com>
2020-08-23 20:37:00 -0400

    Add save method

--- src/models/User.ts
+++ src/models/User.ts
@@ -77,4 +77,12 @@ export class User {
       this.set(response.data);
     });
   }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      });
+  }
 }
