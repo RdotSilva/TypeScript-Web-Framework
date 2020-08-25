56b492ab4792740e0afca762b17a7943da5c1b5f
RdotSilva <RyandotSilva@gmail.com>
2020-08-25 19:57:36 -0400

    Clean up User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,4 @@
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
-import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
+import { Model } from "./Model";
 
 /**
  * Interface listing the properties that a user should have
@@ -20,69 +17,4 @@ const rootUrl = "http://localhost:3000/users";
  * @export
  * @class User
  */
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  /**
-   * Creates an instance of User.
-   * @param {UserProps} attrs - The attributes used to create new User
-   * @memberof User
-   */
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  /**
-   * on method passthrough
-   * @readonly
-   * @memberof User
-   */
-  get on() {
-    return this.events.on;
-  }
-
-  /**
-   * trigger method passthrough
-   * @readonly
-   * @memberof User
-   */
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  /**
-   * get method passthrough
-   * @readonly
-   * @memberof User
-   */
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.attributes.get("id");
-
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without an id");
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger("save");
-      });
-  }
-}
+export class User extends Model<UserProps> {}
