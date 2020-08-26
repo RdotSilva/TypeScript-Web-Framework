415bb939b6e1c181d917efba64ffa03251de7ff9
RdotSilva <RyandotSilva@gmail.com>
2020-08-25 20:10:48 -0400

    Clean up index tests

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,10 @@
 import { User } from "./models/User";
-import axios from "axios";
 
-const user = new User({ name: "Newer Name", age: 0, id: 1 });
+const user = User.buildUser({ id: 1 });
 
 // User on getter method test
-user.on("save", () => {
+user.on("change", () => {
   console.log(user);
 });
 
-user.save();
-
-// Accessors example
-// class Person {
-//   constructor(public firstName: string, public lastName: string) {}
-
-//   get fullName(): string {
-//     return `${this.firstName} ${this.lastName}`;
-//   }
-// }
-
-// const person = new Person("Ryan", "Silva");
-// console.log(person.fullName);
-
-// Create new user with id
-// const userWithId = new User({ id: 1 });
-
-// Update existing user (testing put request)
-// userWithId.set({ name: "Tom", age: 20 });
-// userWithId.save();
-
-// Create new user without id
-// const userWithoutId = new User({ name: "New User", age: 0 });
-// userWithoutId.save();
-
-// Testing the on method to add event callbacks
-// user.events.on("change", () => {
-//   console.log("Change #1");
-// });
-// user.events.on("change", () => {
-//   console.log("Change #2");
-// });
-// user.events.on("click", () => {
-//   console.log("Save was triggered");
-// });
-
-// Here we can update each piece of data individually
-// This is allowed because we added a ? to the UserProps interface to make them optional.
-// user.set({ name: "NewRyan" });
-// user.set({ age: 999 });
-
-// console.log(user.get("name"));
-// console.log(user.get("age"));
-
-// Tests event triggers
-// user.events.trigger("change");
-// user.events.trigger("save");
-// user.events.trigger("");
-
-// Add a user
-// axios.post("http://localhost:3000/users", {
-//   name: "Ryan",
-//   age: 34,
-// });
-
-// Get a user
-// axios.get("http://localhost:3000/1");
+user.fetch();
