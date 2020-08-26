efb0f53b83eacc013860c70d80371c95193ae34e
RdotSilva <RyandotSilva@gmail.com>
2020-08-25 20:00:32 -0400

    Add buildUser static method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
 import { Model } from "./Model";
+import { Attributes } from "./Attributes";
+import { ApiSync } from "./ApiSync";
+import { Eventing } from "./Eventing";
 
 /**
  * Interface listing the properties that a user should have
@@ -17,4 +20,12 @@ const rootUrl = "http://localhost:3000/users";
  * @export
  * @class User
  */
-export class User extends Model<UserProps> {}
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+}
