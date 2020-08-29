3371d2f12d377fb3cb61b0e797adf0a0a8f74e40
RdotSilva <RyandotSilva@gmail.com>
2020-08-29 19:30:44 -0400

    Update eventsMap

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,6 +4,7 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:button": this.onButtonClick,
+      "hover:h1": this.onHeaderHover,
     };
   }
 
@@ -21,11 +22,12 @@ export class UserForm {
      <div> 
       <h1>User Form</h1>
       <input />
+      <button>Click Me</button>
      </div>`;
   }
 
   bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap;
+    const eventsMap = this.eventsMap();
 
     for (let eventKey in eventsMap) {
       const [eventName, selector] = eventKey.split(":");
