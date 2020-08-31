6e1a14b3a3a28e4f24904d21d970ac6c0f87e181
RdotSilva <RyandotSilva@gmail.com>
2020-08-31 18:39:28 -0400

    Add type guard

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -29,9 +29,12 @@ export class UserForm {
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
-    const name = input.value;
-    this.model.set({name});
-  }
+    
+    if (input) {
+      const name = input.value;
+      this.model.set({name});
+    }
+  };
 
   /**
    * Set a random age for the user
