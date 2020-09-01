6b73688a90b9503b55ce220933c9fc4296dc8fc7
RdotSilva <RyandotSilva@gmail.com>
2020-09-01 18:45:41 -0400

    Add extension to UserForm class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,20 +1,6 @@
-import { User } from "../models/User";
+import { View } from "./View";
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  /**
-   * Bind model to on change event handler to re-render any time there is a change
-   * @memberof UserForm
-   */
-  bindModel(): void {
-    this.model.on("change", () => {
-      this.render();
-    });
-  }
-
+export class UserForm extends View {
   /**
    * Create an event map
    * @returns {{ [key: string]: () => void }}
@@ -63,36 +49,5 @@ export class UserForm {
       <button class="set-name">Change Name</button>
       <button class="set-age">Set random age</button>
      </div>`;
-  }
-
-  /**
-   * Bind events to a particular HTML fragment
-   * @param {DocumentFragment} fragment - HTML fragment to bind events to
-   */
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  /**
-   * Create an HTML element out of a template and append it to the parent element
-   * @memberof UserForm
-   */
-  render(): void {
-    this.parent.innerHTML = "";
-
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
   }
 }
