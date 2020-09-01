50f6eb8d114bf04d31aba757f61edbab235dab29
RdotSilva <RyandotSilva@gmail.com>
2020-09-01 18:39:23 -0400

    Add bindModel method

--- src/views/View.ts
+++ src/views/View.ts
@@ -4,4 +4,10 @@ export abstract class View {
   constructor(public parent: Element, public model: User) {
     this.bindModel();
   }
+
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
 }
