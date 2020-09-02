a4493bea460636cb3c1bf385c9bcb98672a33d55
RdotSilva <RyandotSilva@gmail.com>
2020-09-02 17:42:09 -0400

    Update placeholder for name

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -43,10 +43,7 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
      <div> 
-      <h1>User Form</h1>
-      <div>User name: ${this.model.get("name")}</div>
-      <div>User name: ${this.model.get("age")}</div>
-      <input />
+      <input placeholder="${this.model.get("name")}" />
       <button class="set-name">Change Name</button>
       <button class="set-age">Set random age</button>
      </div>`;
