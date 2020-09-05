f3538e7816c056317baea10a1fa0e1f8c22b8769
RdotSilva <RyandotSilva@gmail.com>
2020-09-04 20:06:02 -0400

    Add constructor

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -1,3 +1,5 @@
 import { Collection } from "../models/Collection";
 
-export abstract class CollectionView {}
+export abstract class CollectionView<T, K> {
+  constructor(public parent: Element, public collection: Collection<T, K>) {}
+}
