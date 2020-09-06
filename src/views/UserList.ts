fe7167bfd17a1bc8ac05aab1928fdf98a3beaba7
RdotSilva <RyandotSilva@gmail.com>
2020-09-05 20:20:45 -0400

    Add JSDoc comments

--- src/views/UserList.ts
+++ src/views/UserList.ts
@@ -3,6 +3,12 @@ import { User, UserProps } from "../models/User";
 import { UserShow } from "./UserShow";
 
 export class UserList extends CollectionView<User, UserProps> {
+  /**
+   * Create a new view using a model and render that to an item parent
+   * @param {User} model - Model used to create view
+   * @param {Element} itemParent - Parent element to render view to
+   * @memberof UserList
+   */
   renderItem(model: User, itemParent: Element): void {
     new UserShow(itemParent, model).render();
   }
